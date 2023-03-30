174eb88f99a2691e12fcf247b752784aebf42fd6
Gaurav <mahendrakergaurav@gmail.com>
2023-03-30 19:50:09 +0530

    second commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-if(process.env.NODE_ENV !== 'production'){
-    require('dotenv').parse()
-}
+// if(process.env.NODE_ENV !== 'production'){
+//     require('dotenv').parse()
+// }
 
 const express= require('express')
 const app= express()
@@ -14,13 +14,22 @@ app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
-const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true})
+// const mongoose = require('mongoose')
+// mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true})
 
-const db= mongoose.connection
+// const db= mongoose.connection
 
-db.on('error', error=>console.log(error))
-db.once('open',()=>console.log('Connected to Datbase'))
+// db.on('error', error=>console.log(error))
+// db.once('open',()=>console.log('Connected to Datbase'))
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://mahendrakergaurav:gauravaryasonakshi@cluster0.cdycnzq.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.use('/', indexRouter)
 app.listen( process.env.PORT || 3000)
