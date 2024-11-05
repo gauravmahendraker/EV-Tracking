07eb661dd47dfbd9fdb829c8a2f9dea60cb6b22d
gauravmahendraker <104026985+gauravmahendraker@users.noreply.github.com>
2024-11-05 17:54:11 +0530

    Update server.js
    Pushed the code such that the app works on multiple server instance, which i had tried earlier but forgot to push

--- server.js
+++ server.js
@@ -1,78 +1,76 @@
 
 
-// if(process.env.NODE_ENV !== 'production'){
-//     require('dotenv').parse()
-// }
-
-const express= require('express')
-const app= express()
+const express = require('express');
+const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-const expressLayouts = require('express-ejs-layouts')
-// const bodyParser = require('body-parser')
-// var http = require('node:http');
-var bodyParser = require('body-parser')
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const { createClient } = require('redis');
+const { createAdapter } = require('@socket.io/redis-adapter');
 
+// Redis setup
+const redisClient = createClient(); // Create Redis client for publishing
+const redisSubscriber = createClient(); // Create Redis client for subscribing
+
+redisClient.connect();
+redisSubscriber.connect();
+
+redisClient.on('error', (err) => console.error('Redis client error:', err));
+redisSubscriber.on('error', (err) => console.error('Redis subscriber error:', err));
+
+// Use Redis as the adapter for Socket.IO
+io.adapter(createAdapter(redisClient, redisSubscriber));
+
+// Middleware setup
 app.use(express.static(__dirname + '/assets'));
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'));
 
-// parse application/json
-app.use(bodyParser.json())
 
-const indexRouter = require('./routes/index')
-const driverRouter = require('./routes/drivers')
-const adminRouter = require('./routes/admin')
+const indexRouter = require('./routes/index');
+const driverRouter = require('./routes/drivers');
+const adminRouter = require('./routes/admin');
 
-app.set('view engine','ejs')
-app.set('views', __dirname + '/views')
-app.set('layout','layouts/layout')
-app.use(expressLayouts)
-app.use(express.static('public'))
-// app.use(bodyParser.urlencoded({limit:'10mb',extended: false}))
-// app.locals.http = http
 
- const mongoose = require('mongoose')
-// mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true})
+const uri = "mongodb+srv://mahendrakergaurav:gauravaryasonakshi@cluster0.cdycnzq.mongodb.net/test1?retryWrites=true&w=majority";
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch((e) => console.error('MongoDB connection error:', e));
 
-// const db= mongoose.connection
-
-// db.on('error', error=>console.log(error))
-// db.once('open',()=>console.log('Connected to Datbase'))
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('sendLocation',(location)=>{
-      //  console.log(location)
-        var loc = location
-        socket.broadcast.emit("driverLocation",loc)
-    })
-  });
+    console.log('A user connected');
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://mahendrakergaurav:gauravaryasonakshi@cluster0.cdycnzq.mongodb.net/test1?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// }).then(()=>console.log('connected to mongodb'))
-// .catch(e=>console.log(e));
-// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).then(console.log("connected"))
-mongoose.connect(uri,{
-    useNewUrlParser: true,
-   
-    useUnifiedTopology: true
+    socket.on('sendLocation', (location) => {
+        redisClient.publish('driverLocation', JSON.stringify(location));
+    });
 });
 
-app.use('/', indexRouter)
-app.use('/drivers',driverRouter)
-app.use('/admin',adminRouter)
 
+redisSubscriber.subscribe('driverLocation', (message) => {
+    const location = JSON.parse(message);
+    io.emit('driverLocation', location);
+});
 
-server.listen( process.env.PORT || 3001)
-// app.listen(3000)
+// Routes
+app.use('/', indexRouter);
+app.use('/drivers', driverRouter);
+app.use('/admin', adminRouter);
 
- console.log('Server running at http://localhost:3001');
+// Start the server
+server.listen(process.env.PORT || 3001, () => {
+    console.log('Server running at http://localhost:3001');
+});
